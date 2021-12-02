733383ab1448a5bd4336ad115c650ea02de0eb2e
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-02 17:14:09 +0900

    01330 두 수 비교하기

---
+++ 01330 두 수 비교하기/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01330_두_수_비교하기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] arrNumber = a.Split(" ");
+
+            int A = int.Parse(arrNumber[0]);
+            int B = int.Parse(arrNumber[1]);
+
+            if (A > B)
+            {
+                Console.WriteLine(">");
+            }
+            else if (A < B)
+            {
+                Console.WriteLine("<");
+            }
+            else
+            {
+                Console.WriteLine("==");
+            }
+        }
+    }
+}
