b3944a2a37c9575406f201372ea45f4f15535da9
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 12:51:28 +0900

    02742 기찍 N

---
+++ 02742 기찍 N/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+namespace _02742_기찍_N
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = N; i >= 1; i--)
+            {
+                sb.Append(i + "\n");
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
