30e8d22a4bb0384ac929707ba55e5246e4a4df5b
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 12:16:37 +0900

    08393 합

---
+++ 08393 합/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08393_합
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= T; i++)
+            {
+                sum += i;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
