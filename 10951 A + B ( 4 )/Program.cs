970629a3493649e2af782ca2e0e6330299c1372b
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 19:50:52 +0900

    10951 A + B ( 4 )

---
+++ 10951 A + B ( 4 )/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10951_A___B___4__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input))
+                {
+                    break;
+                }
+
+                int[] refinedInput = Array.ConvertAll(input.Split(" "), value => int.Parse(value));
+
+                Console.WriteLine((refinedInput[0] + refinedInput[1]));
+            }
+        }
+    }
+}
