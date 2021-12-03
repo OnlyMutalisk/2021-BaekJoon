4e382943babeb78928a8db14e53b8e045a47ca90
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 16:09:41 +0900

    11022 A + B ( 8 )

---
+++ 11022 A + B ( 8 )/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _11022_A___B___8__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            string[] input = new string[2];
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < T; i++)
+            {
+                input = Console.ReadLine().Split(" ");
+
+                sb.Append(String.Format("Case #{0}: {1} + {2} = {3}\n", i + 1, input[0], input[1], (int.Parse(input[0]) + int.Parse(input[1]))));
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
