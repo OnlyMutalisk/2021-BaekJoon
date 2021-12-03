16b60301e40ab7c1eb00f12d5c2774977a69a0d6
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 19:26:07 +0900

    10871 X 보다 작은 수

---
+++ 10871 X 보다 작은 수/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _10871_X_보다_작은_수
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+
+            int N = int.Parse(input[0]);
+            int X = int.Parse(input[1]);
+
+            int[] input2 = Array.ConvertAll(Console.ReadLine().Split(" "), value => int.Parse(value));
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in input2)
+            {
+                if (item < X)
+                {
+                    sb.Append($"{item} ");
+                }
+            }
+
+            Console.WriteLine(sb);
+        }
+    }
+}
