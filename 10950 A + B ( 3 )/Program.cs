5e36f2688d134736d2f7d39c62ff863e74ff6854
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 12:11:09 +0900

    10950 A + B ( 3 )

---
+++ 10950 A + B ( 3 )/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10950_A___B___3__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                int A = int.Parse(input[0]);
+                int B = int.Parse(input[1]);
+
+                Console.WriteLine(A + B);
+            }
+        }
+    }
+}
