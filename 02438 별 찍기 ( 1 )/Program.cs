153830785cef992f122f9b1d28223bad47244973
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-03 16:14:25 +0900

    02438 별 찍기 ( 1 )

---
+++ 02438 별 찍기 ( 1 )/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02438_별_찍기___1__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
