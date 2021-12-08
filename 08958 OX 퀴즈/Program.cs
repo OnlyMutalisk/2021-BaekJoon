bc1ae744c844be4cbf2e4e3b4873511e80a9a55b
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-08 18:25:21 +0900

    08958 OX 퀴즈

---
+++ 08958 OX 퀴즈/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _08958_OX_퀴즈
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int trashValue = int.Parse(Console.ReadLine());
+            int[] arrScore = new int[trashValue];
+
+            for (int i = 0; i < trashValue; i++)
+            {
+                string answer = Console.ReadLine();
+
+                int count = 0;
+                int score = 0;
+
+                foreach (var item in answer)
+                {
+                    if (item.ToString().Equals("O"))
+                    {
+                        count++;
+                        score += count;
+                    }
+                    else
+                    {
+                        count = 0;
+                    }
+                }
+
+                arrScore[i] = score;
+            }
+
+            foreach (var item in arrScore)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
