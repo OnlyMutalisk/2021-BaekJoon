edc1450676714382aa914230ca0cad19cb4d5b2f
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-08 14:24:17 +0900

    02562 최댓값

---
+++ 02562 최댓값/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02562_최댓값
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = new int[9];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] arr = (int[])input.Clone();
+
+            Array.Sort(arr);
+
+            int MAX = arr[arr.Length - 1];
+            int index = Array.IndexOf(input, MAX) + 1;
+
+            Console.WriteLine(MAX);
+            Console.WriteLine(index);
+        }
+    }
+}
