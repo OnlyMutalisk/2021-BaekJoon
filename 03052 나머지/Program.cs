9bb7432dec968ecec9a42cf880eacbbc2c05c956
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-08 17:46:48 +0900

    03052 나머지

---
+++ 03052 나머지/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03052_나머지
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listRemainder = new List<int>();
+                
+            for (int i = 0; i < 10; i++)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int remainder = a % 42;
+
+                listRemainder.Add(remainder);
+            }
+
+            listRemainder = listRemainder.Distinct().ToList();
+
+            Console.WriteLine(listRemainder.Count);
+        }
+    }
+}
