8bed2ae758a4a8370dd2ebe991d2b2acc65c733e
Onlymutalisk <onlymutalisk@gmail.com>
2021-12-08 12:51:17 +0900

    10818 최소, 최대

---
+++ 10818 최소, 최대/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _10818_최소__최대
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+
+            string[] input = Console.ReadLine().Split(" ");
+
+            int[] arrN = Array.ConvertAll(input, value => int.Parse(value));
+
+            Array.Sort(arrN);
+
+            Console.WriteLine(arrN[0] + " " + arrN[arrN.Length - 1]);
+        }
+    }
+}
